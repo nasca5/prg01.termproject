d1aaf7f57244ad474e6da1d9e23228ea131cbafc
nasca5 <nasca55@naver.com>
2021-11-04 07:51:21 +0900

    1. 프로그램 작성에 필요한 변수 2. 프로그램 작성에 필요한 함수 3. user 구조체와 stone 구조체 정의 4. start 함수와 restart 함수, reset_game 함수 정의

--- prg01.termproject/prg01.termproject.c
+++ prg01.termproject/prg01.termproject.c
@@ -1,18 +1,175 @@
-#include <stdio.h>
+# include <stdio.h>
+# include <Windows.h>
+# include <conio.h>
+# include <stdbool.h>
+# include <time.h>
+
+# define WIDTH 20
+# define HEIGHT 12
+
+void start();
+void restart();
+void make_bad();
+void make_good();
+void make_stone();
+void move_stone();
+void remove_stone();
+void move_user();
+void display();
+bool reset_game();
+
+
+
+typedef struct {
+	
+	int x;
+	int y;
+	bool exist;
+
+} STONE ;
+
+typedef struct {
+
+	int x;
+
+} USER;
+
+int set = 100;
+int score;
+int life;
+int result;
+
+bool play = TRUE;
+
+STONE stone[WIDTH];
+USER user;
+
+void start() {
+	
+	int score = 0;
+	int life = 3;
+
+	for (int i = 0; i < WIDTH; i++) {
+
+		stone[i].exist = FALSE;
+		user.x = WIDTH / 2;
+
+	}
+
+}
+
+void make_stone() {
+
+}
+
+void move_stone() {
+
+}
+
+void remove_stone() {
+
+}
+
+void make_good() {
+
+}
+
+void make_bad() {
+
+}
+
+void move_user() {
+
+}
+
+void display() {
+
+}
+
+bool reset_game() {
+
+	if (life == 0) {
+		
+		printf("목숨이 0개입니다.\n");
+
+		return TRUE;
+	
+	}
+
+	else if (score == 10) {
+
+		printf("점수가 10점이 되었습니다! 축하드립니다!\n");
+		return TRUE;
+
+	}
+
+	return FALSE;
+	
+}
+
+
+
+
+void restart() {
+
+	while (true) {
+		
+		printf("다시 하시겠습니까? (Y = 1 / N = 0) : ");
+		scanf_s("%d", &result);
+
+		if ((result == 1) || (result == 0))
+			break;
+
+		printf("1(Yes) or 0(No)으로 입력해주세요.\n");
+			
+	}
+
+	if (result == 1) {
+
+		printf("game restart");
+		start();
+
+	}
+
+	else {
+
+		play = FALSE;
+		printf("game end");
+
+	}
+
+}
 
 int main(void) {
 
-	int a = 0;
+	start();
 
-	int* ab;
+	while (play) {
 
-	ab = &a;
+		make_stone();
+
+		move_stone();
+
+		remove_stone();
+
+		move_user();
+
+		make_bad();
+
+		make_good();
+
+		display();
 	
-	printf("ab = ");
+		if (reset_game()) {
 
-	scanf_s("%d", &(*ab));
+			restart();
 
-	printf("a = %d", a);
+		}
+
+		Sleep(set);
+		system("cls");
+
+	}
 
 	return 0;
 
